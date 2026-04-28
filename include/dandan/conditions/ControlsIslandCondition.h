74977d91dac469d0dc69aacf16eec111c5b270a4
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-04-29 01:03:55 +0200

    updated doxygen workflow to fail on warning

--- include/dandan/conditions/ControlsIslandCondition.h
+++ include/dandan/conditions/ControlsIslandCondition.h
@@ -9,6 +9,7 @@ namespace dandan::conditions
     class ControlsIslandCondition : public ICondition
     {
     public:
+        /// Checks if the player controls the island.
         bool isSatisfied(const core::Game &game) override;
     };
 } // namespace dandan::conditions
