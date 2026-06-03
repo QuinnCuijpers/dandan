351756c2d6fddd4cf3ded1401e82e81238fd9acb
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-06-03 22:13:18 +0200

    Added tests for full ability displays

--- tests/integration/AbilityDisplayTest.cpp
+++ tests/integration/AbilityDisplayTest.cpp
@@ -1,15 +1,8 @@
-#include "dandan/abilities/StaticAbility.h"
-#include "gtest/gtest.h"
-#include <array>
 #ifdef DANDAN_SERIALIZE
+
 #include "common.h"
 #include "dandan/dandan.h"
-#include <nlohmann/json.hpp>
-#include <string>
-
-static constexpr std::array<const char *, 8> activable_cards = {
-    "Island",      "Halimar Depths", "Izzet Boilerworks", "Lonely Sandbar",
-    "Remote Isle", "Shivan Reef",    "Svyelunite Temple", "Temple of Epiphany"};
+#include <gtest/gtest.h>
 
 struct AbilityDisplayCase
 {
@@ -41,10 +34,7 @@ TEST_P(AbilityDisplayTest, DisplaysExpectedActivatableAbilities)
         {
             continue;
         }
-        for (size_t i{0}; i < ability->optionsAmount(); ++i)
-        {
-            actual += ability->displayOption(i) + '\n';
-        }
+        actual += ability->display() + '\n';
     }
     EXPECT_EQ(actual, test_case.expected_display);
 }
@@ -52,51 +42,11 @@ TEST_P(AbilityDisplayTest, DisplaysExpectedActivatableAbilities)
 INSTANTIATE_TEST_SUITE_P(
     ActivatableCards, AbilityDisplayTest,
     ::testing::Values(
-        AbilityDisplayCase{"Island", "[T]: Add U\n"},
-        AbilityDisplayCase{"Halimar Depths", "[T]: Add U\n"},
-        AbilityDisplayCase{"Izzet Boilerworks", "[T]: Add UR\n"},
-        AbilityDisplayCase{"Lonely Sandbar",
-                           "[T]: Add U\nU, Discard this card: Draw a card\n"},
-        AbilityDisplayCase{"Remote Isle",
-                           "[T]: Add U\n(2), Discard this card: Draw a card\n"},
         AbilityDisplayCase{
             "Shivan Reef",
-            "[T]: Add C\n[T]: Add U. This Card deals 1 damage to you\n"
-            "[T]: Add R. This Card deals 1 damage to you\n"},
-        AbilityDisplayCase{"Svyelunite Temple",
-                           "[T]: Add U\n[T], Sacrifice this Card: Add UU\n"},
-        AbilityDisplayCase{"Temple of Epiphany", "[T]: Add U\n[T]: Add R\n"}),
+            "[T]: Add C\n[T]: Add U or R. This Card deals 1 damage to you\n"},
+        AbilityDisplayCase{"Temple of Epiphany", "[T]: Add U or R\n"}),
     [](const ::testing::TestParamInfo<AbilityDisplayCase> &info)
     { return CardParamName(info.param.card_name); });
-
-TEST(AbilityDisplayCardsTest, ActivatableListAndCasesStayAligned)
-{
-    static constexpr std::array<AbilityDisplayCase, activable_cards.size()>
-        cases = {
-            AbilityDisplayCase{"Island", "[T]: Add U\n"},
-            AbilityDisplayCase{"Halimar Depths", "[T]: Add U\n"},
-            AbilityDisplayCase{"Izzet Boilerworks", "[T]: Add UR\n"},
-            AbilityDisplayCase{
-                "Lonely Sandbar",
-                "[T]: Add U\nU, Discard this card: Draw a card\n"},
-            AbilityDisplayCase{
-                "Remote Isle",
-                "[T]: Add U\n(2), Discard this card: Draw a card\n"},
-            AbilityDisplayCase{
-                "Shivan Reef",
-                "[T]: Add C\n[T]: Add U. This Card deals 1 damage to you\n"
-                "[T]: Add R. This Card deals 1 damage to you\n"},
-            AbilityDisplayCase{
-                "Svyelunite Temple",
-                "[T]: Add U\n[T], Sacrifice this Card: Add UU\n"},
-            AbilityDisplayCase{"Temple of Epiphany",
-                               "[T]: Add U\n[T]: Add R\n"},
-        };
-
-    for (size_t i{}; i < activable_cards.size(); ++i)
-    {
-        EXPECT_STREQ(activable_cards.at(i), cases.at(i).card_name);
-    }
-}
 
 #endif
