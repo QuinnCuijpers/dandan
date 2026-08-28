fce4605bdaa9aedc505cab0008924da7aa156f3c
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-08-28 20:36:31 +0200

    update Execution context to contain game state and all the managers

--- include/dandan/core/ExecutionContext.h
+++ include/dandan/core/ExecutionContext.h
@@ -3,18 +3,29 @@
 
 #include "dandan/core/CardRegistry.h"
 #include <functional>
+#include <istream>
 
 namespace dandan::core
 {
-    class Game;
-} // namespace dandan::core
+    class GameState;
+    class EventManager;
+    class ConditionManager;
+    class PreventionManager;
+    class PriorityManager;
+    class ReplacementManager;
+    class SBAManager;
 
-namespace dandan::core
-{
     struct ExecutionContext
     {
-        std::reference_wrapper<core::Game> state;
+        std::reference_wrapper<core::GameState> state;
         std::reference_wrapper<core::CardRegistry> cards;
+        std::reference_wrapper<std::istream> input;
+        std::reference_wrapper<EventManager> event_manager;
+        std::reference_wrapper<PreventionManager> prevention_manager;
+        std::reference_wrapper<ReplacementManager> replacement_manager;
+        std::reference_wrapper<PriorityManager> priority_manager;
+        std::reference_wrapper<ConditionManager> condition_manager;
+        std::reference_wrapper<SBAManager> sba_manager;
     };
 } // namespace dandan::core
 #endif
