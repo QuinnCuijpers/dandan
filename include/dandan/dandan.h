925958b2e1ef5e53f62426c220fb6c5187f0086c
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-09-01 15:44:08 +0200

    update dandan.h

--- include/dandan/dandan.h
+++ include/dandan/dandan.h
@@ -3,29 +3,48 @@
 
 #include "dandan/abilities/ActivatedAbility.h"
 #include "dandan/abilities/EventTriggeredAbility.h"
-#include "dandan/abilities/IAbility.h"
 #include "dandan/abilities/ManaAbility.h"
 #include "dandan/abilities/SpellAbility.h"
-#include "dandan/abilities/StateTriggeredAbility.h"
 #include "dandan/abilities/StaticAbility.h"
 #include "dandan/abilities/WithDamage.h"
+#include "dandan/conditions/DefenderControlsNoBasicCondition.h"
+#include "dandan/conditions/FlyingCondition.h"
+#include "dandan/conditions/MatchesReadLinksCondition.h"
+#include "dandan/conditions/SelfControlsNoBasicCondition.h"
 #include "dandan/core/Card.h"
 #include "dandan/core/CardData.h"
 #include "dandan/core/Game.h"
+#include "dandan/costs/AndCost.h"
 #include "dandan/costs/CyclingCost.h"
 #include "dandan/costs/ICost.h"
 #include "dandan/costs/ManaCost.h"
 #include "dandan/costs/SelfSacrificeCost.h"
 #include "dandan/effects/continuous/replacement/EntersTappedEffect.h"
 #include "dandan/effects/continuous/replacement/IReplacementEffect.h"
+#include "dandan/effects/one_shot/BounceEffect.h"
 #include "dandan/effects/one_shot/BounceLandEffect.h"
+#include "dandan/effects/one_shot/ChangeCharasticsEffect.h"
+#include "dandan/effects/one_shot/ChangeLandTypeEffect.h"
+#include "dandan/effects/one_shot/ChooseCardNameAndMillEffect.h"
 #include "dandan/effects/one_shot/DrawEffect.h"
 #include "dandan/effects/one_shot/ETBEffect.h"
+#include "dandan/effects/one_shot/ExileTopEffect.h"
 #include "dandan/effects/one_shot/IOneShotEffect.h"
+#include "dandan/effects/one_shot/MemoryLapseEffect.h"
+#include "dandan/effects/one_shot/MindBendEffect.h"
+#include "dandan/effects/one_shot/OptionalDrawEffect.h"
 #include "dandan/effects/one_shot/PeekEffect.h"
+#include "dandan/effects/one_shot/PutCardOnTopEffect.h"
 #include "dandan/effects/one_shot/ScryEffect.h"
+#include "dandan/effects/one_shot/SelfSacrificeEffect.h"
+#include "dandan/effects/one_shot/ShowAndTellEffect.h"
+#include "dandan/effects/one_shot/SpinToTopEffect.h"
+#include "dandan/effects/one_shot/TimeTwisterEffect.h"
+#include "dandan/effects/one_shot/TutorTopEffect.h"
 #include "dandan/events/ETBEvent.h"
 #include "dandan/mana/ManaList.h"
+#include "dandan/numbers/ConditionalNumber.h"
+#include "dandan/numbers/GraveyardCount.h"
 #include "dandan/triggers/ETBtrigger.h"
 
 namespace dandan
@@ -44,13 +63,31 @@ namespace dandan
     using core::Game;
     using core::Stats;
 
+    using conditions::DefenderControlsNoBasicCondition;
+    using conditions::FlyingCondition;
+    using conditions::MatchesReadLinksCondition;
+    using conditions::SelfControlsNoBasicCondition;
+
+    using effects::BounceEffect;
     using effects::BounceLandEffect;
+    using effects::ChangeCharacteristicsEffect;
+    using effects::ChangeLandTypeEffect;
+    using effects::ChooseCardNameAndMillEffect;
     using effects::DrawEffect;
-    using effects::DrawEffectDefinition;
     using effects::ETBEffect;
+    using effects::ExileTopEffect;
     using effects::IOneShotEffect;
+    using effects::MemoryLapseEffect;
+    using effects::MindBendEffect;
+    using effects::OptionalDrawEffect;
     using effects::PeekEffect;
+    using effects::PutCardOnTopEffect;
     using effects::ScryEffect;
+    using effects::SelfSacrificeEffect;
+    using effects::ShowAndTellEffect;
+    using effects::SpinToTopEffect;
+    using effects::TimeTwisterEffect;
+    using effects::TutorTopEffect;
 
     using effects::EntersTappedEffect;
     using effects::IReplacementEffect;
@@ -61,6 +98,7 @@ namespace dandan
     using triggers::ETBTrigger;
     using triggers::SelfETBTrigger;
 
+    using costs::AndCost;
     using costs::CyclingCost;
     using costs::ICost;
     using costs::ManaCost;
@@ -69,6 +107,9 @@ namespace dandan
 
     using mana::ManaList;
     using mana::ManaPrice;
+
+    using numbers::ConditionalNumber;
+    using numbers::GraveyardCount;
 
 } // namespace dandan
 
