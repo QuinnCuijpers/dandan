b074850ca02c803dba14b4e8c94f646da2800285
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-09-02 16:46:33 +0200

    move setup function to cpp file

--- tests/unit/serialization/DeserializeTest.cpp
+++ tests/unit/serialization/DeserializeTest.cpp
@@ -1,6 +1,25 @@
 #include <gtest/gtest.h>
 #ifdef DANDAN_SERIALIZE
 #include "DeserializeTest.h"
+#include <fstream>
+
+void DeserializeTest::SetUp()
+{
+    const auto &params{GetParam()};
+    const auto card = params;
+    std::string_view name{card.getData().name};
+
+    auto json_file_path{std::filesystem::path{DANDAN_PROJECT_SOURCE} /
+                        "data/jsons" / name};
+    json_file_path += ".json";
+
+    std::ifstream file{json_file_path};
+    nlohmann::json json{};
+    file >> json;
+
+    m_expected = json;
+    m_received = nlohmann::json(card.getData());
+}
 
 TEST_P(DeserializeTest, DeserializeCorrect)
 {
--- tests/unit/serialization/DeserializeTest.h
+++ tests/unit/serialization/DeserializeTest.h
@@ -5,7 +5,6 @@
 
 #include "dandan/dandan.h"
 #include "gtest/gtest.h"
-#include <fstream>
 #include <nlohmann/json.hpp>
 
 class DeserializeTest : public testing::TestWithParam<dandan::Card>
@@ -16,23 +15,7 @@ public:
     nlohmann::json m_received;
 
 protected:
-    void SetUp() override
-    {
-        const auto &params{GetParam()};
-        const auto card = params;
-        std::string_view name{card.getData().name};
-
-        auto json_file_path{std::filesystem::path{DANDAN_PROJECT_SOURCE} /
-                            "data/jsons" / name};
-        json_file_path += ".json";
-
-        std::ifstream file{json_file_path};
-        nlohmann::json json{};
-        file >> json;
-
-        m_expected = json;
-        m_received = nlohmann::json(card.getData());
-    }
+    void SetUp() override;
 };
 
 #endif
