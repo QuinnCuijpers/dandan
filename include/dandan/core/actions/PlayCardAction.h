94b65e6388a9e661aad4c84c03b83e6173a45ed9
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-09-12 12:38:48 +0200

    add castmode and cast request to action

--- include/dandan/core/actions/PlayCardAction.h
+++ include/dandan/core/actions/PlayCardAction.h
@@ -1,10 +1,61 @@
 #ifndef DANDAN_PLAYCARDACTION_H
 #define DANDAN_PLAYCARDACTION_H
 
+#include "dandan/core/CardID.h"
 #include "dandan/core/Game.h"
 #include "dandan/core/actions/IAction.h"
 #include "dandan/effects/one_shot/IOneShotEffect.h"
+#include "dandan/utils/strSplit.h"
+#include <cassert>
+#include <cstdint>
 #include <memory>
+#include <optional>
+#include <string>
+
+enum class CastMode : uint8_t
+{
+    Normal,
+    Flashback,
+};
+
+inline CastMode CastModeFromStr(const std::string &str)
+{
+    if (str == "Flashback")
+    {
+        return CastMode::Flashback;
+    }
+
+    return CastMode::Normal;
+}
+
+struct CastRequest
+{
+
+    CastRequest(dandan::core::CardID card_id,
+                std::optional<CastMode> mode = std::nullopt)
+        : card_id(card_id), mode{mode.value_or(CastMode::Normal)}
+    {
+    }
+
+    dandan::core::CardID card_id;
+    CastMode mode = CastMode::Normal;
+
+    static CastRequest fromStr(const std::string &input)
+    {
+        auto parts = strSplit(input);
+        assert(parts[0] == "play");
+        auto card_id{dandan::core::CardID::fromInt(std::stoi(parts[1]))};
+
+        std::optional<CastMode> mode{std::nullopt};
+
+        for (size_t pos{}; pos < parts.size(); ++pos)
+        {
+            mode = CastModeFromStr(parts[pos]);
+        }
+
+        return {card_id, mode};
+    };
+};
 
 namespace dandan::core
 {
@@ -19,9 +70,9 @@ namespace dandan::core
     public:
         /**
          * Constructs a new play card action.
-         * @param card_id The card_id of the card to play.
+         * @param req The cast request of the card to play.
          */
-        explicit PlayCardAction(CardID card_id);
+        explicit PlayCardAction(CastRequest req);
 
         /** Get the card ID of the card to play.
          * @return The card ID of the card to play.
@@ -32,7 +83,7 @@ namespace dandan::core
             core::ExecutionContext exec_ctx) override;
 
     private:
-        CardID m_card_id;
+        CastRequest m_cast_request;
     };
 } // namespace dandan::core
 
