f7cced7ef8dc70e9ef21191356c8eb7bbf4035f4
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-09-13 17:05:07 +0200

    integrate changes into the stack

--- include/dandan/core/Stack.h
+++ include/dandan/core/Stack.h
@@ -2,17 +2,16 @@
 #define DANDAN_CORE_STACK_H
 
 #include "dandan/abilities/BoundAbility.h"
-#include "dandan/abilities/IAbility.h"
-#include "dandan/effects/one_shot/IOneShotEffect.h"
+#include "dandan/core/CardID.h"
+#include "dandan/core/CastContext.h"
 #include <algorithm>
-#include <iostream>
 #include <variant>
 #include <vector>
 
 namespace dandan::core
 {
 
-    using StackObject = std::variant<CardID, abilities::BoundAbility>;
+    using StackObject = std::variant<CastContext, abilities::BoundAbility>;
 
     /** @brief The class that represents the stack that holds effects that
      * are about to `apply`
@@ -26,19 +25,12 @@ namespace dandan::core
         /** Pushes a StackObject onto the stack.
          * @param object The object to push.
          */
-        void push(const StackObject &object)
-        {
-            std::cout << "Pushing object onto stack\n";
-            m_stack.push_back(object);
-        }
+        void push(const StackObject &object);
 
         /** Checks if the stack is empty.
          * @return True if the stack is empty, false otherwise.
          */
-        [[nodiscard]] bool isEmpty() const
-        {
-            return m_stack.empty();
-        }
+        [[nodiscard]] bool isEmpty() const;
 
         /** Resolves the next object in the stack and pops it from the stack.
          * @param game The game instance.
@@ -48,22 +40,25 @@ namespace dandan::core
         /** Gets the stack objects immutably.
          * @return A const reference to the vector of stack objects.
          */
-        [[nodiscard]] const std::vector<StackObject> &getStackObjects() const
-        {
-            return m_stack;
-        }
+        [[nodiscard]] const std::vector<StackObject> &getStackObjects() const;
 
-        void removeObject(const StackObject &object)
+        void removeObject(const StackObject &object);
+
+        void removeCard(CardID card_id)
         {
-            m_stack.erase(std::remove_if(m_stack.begin(), m_stack.end(),
-                                         [&](const StackObject &stack_object)
-                                         { return stack_object == object; }),
-                          m_stack.end());
+            m_stack.erase(
+                std::remove_if(
+                    m_stack.begin(), m_stack.end(),
+                    [card_id](const StackObject &object)
+                    {
+                        return std::holds_alternative<CastContext>(object) &&
+                               std::get<CastContext>(object).card_id == card_id;
+                    }),
+                m_stack.end());
         }
 
     private:
         std::vector<StackObject> m_stack;
     };
 } // namespace dandan::core
-
 #endif
