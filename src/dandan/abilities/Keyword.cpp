534db9c15a140bf0258067c3c6e2fa989a62b730
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-09-24 13:45:31 +0200

    comment out unused functions

--- src/dandan/abilities/Keyword.cpp
+++ src/dandan/abilities/Keyword.cpp
@@ -14,24 +14,24 @@ namespace dandan::abilities
 
 namespace dandan::core
 {
-    bool isFlyingAbility(const dandan::abilities::IAbility &ability)
-    {
-        const auto *static_ability =
-            dynamic_cast<const dandan::abilities::StaticAbility *>(&ability);
-        if ((static_ability == nullptr) ||
-            static_ability->getType() !=
-                dandan::abilities::StaticAbility::Type::Prevention)
-        {
-            return false;
-        }
-        const auto *effect =
-            dynamic_cast<const dandan::effects::BlockPreventionEffect *>(
-                static_ability->getEffect());
-        if (effect == nullptr)
-        {
-            return false;
-        }
-        return dynamic_cast<const dandan::conditions::FlyingCondition *>(
-                   effect->getCondition()) != nullptr;
-    }
+    // bool isFlyingAbility(const dandan::abilities::IAbility &ability)
+    // {
+    //     const auto *static_ability =
+    //         dynamic_cast<const dandan::abilities::StaticAbility *>(&ability);
+    //     if ((static_ability == nullptr) ||
+    //         static_ability->getType() !=
+    //             dandan::abilities::StaticAbility::Type::Prevention)
+    //     {
+    //         return false;
+    //     }
+    //     const auto *effect =
+    //         dynamic_cast<const dandan::effects::BlockPreventionEffect *>(
+    //             static_ability->getEffect());
+    //     if (effect == nullptr)
+    //     {
+    //         return false;
+    //     }
+    //     return dynamic_cast<const dandan::conditions::FlyingCondition *>(
+    //                effect->getCondition()) != nullptr;
+    // }
 } // namespace dandan::core
