5f3bd9093ccb7b4c9e0938850352059d61bfdff6
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-09-24 14:57:33 +0200

    const correctness

--- include/dandan/effects/one_shot/ChangeCharasticsEffect.h
+++ include/dandan/effects/one_shot/ChangeCharasticsEffect.h
@@ -147,10 +147,10 @@ namespace dandan::serialization::registration
 
                     const auto &color = characteristics_json["color"];
                     const auto &subtypes = characteristics_json["subtypes"];
-                    auto base_power = characteristics_json["base_power"];
-                    auto base_thoughness =
+                    const auto &base_power = characteristics_json["base_power"];
+                    const auto &base_thoughness =
                         characteristics_json["base_thoughness"];
-                    auto stats{Stats{base_power, base_thoughness}};
+                    const auto &stats{Stats{base_power, base_thoughness}};
                     const auto &loses_all_abilities =
                         characteristics_json["loses_all_abilities"];
 
